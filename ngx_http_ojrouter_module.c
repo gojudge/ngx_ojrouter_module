55a52091872a7a7f8a18b96294b27393ff39009a
Rex Lee(李俊) <duguying2008@gmail.com>
2016-06-08 17:10:00 +0800

    fix

--- ngx_http_ojrouter_module.c
+++ ngx_http_ojrouter_module.c
@@ -8,47 +8,47 @@
 
 typedef struct {
     ngx_str_t output_words;
-} ngx_http_hello_world_loc_conf_t;
+} ngx_http_ojrouter_loc_conf_t;
 
 // To process HelloWorld command arguments
-static char* ngx_http_hello_world(ngx_conf_t* cf, ngx_command_t* cmd, void* conf);
+static char* ngx_http_ojrouter(ngx_conf_t* cf, ngx_command_t* cmd, void* conf);
 
 // Allocate memory for HelloWorld command
-static void* ngx_http_hello_world_create_loc_conf(ngx_conf_t* cf);
+static void* ngx_http_ojrouter_create_loc_conf(ngx_conf_t* cf);
 
 // Copy HelloWorld argument to another place
-static char* ngx_http_hello_world_merge_loc_conf(ngx_conf_t* cf, void* parent, void* child);
+static char* ngx_http_ojrouter_merge_loc_conf(ngx_conf_t* cf, void* parent, void* child);
 
 // Structure for the HelloWorld command
-static ngx_command_t ngx_http_hello_world_commands[] = {
+static ngx_command_t ngx_http_ojrouter_commands[] = {
         {
-                ngx_string("hello_world"), // The command name
+                ngx_string("ojrouter"), // The command name
                 NGX_HTTP_LOC_CONF | NGX_CONF_TAKE1,
-                ngx_http_hello_world, // The command handler
+                ngx_http_ojrouter, // The command handler
                 NGX_HTTP_LOC_CONF_OFFSET,
-                offsetof(ngx_http_hello_world_loc_conf_t, output_words),
+                offsetof(ngx_http_ojrouter_loc_conf_t, output_words),
                 NULL
         },
         ngx_null_command
 };
 
 // Structure for the HelloWorld context
-static ngx_http_module_t ngx_http_hello_world_module_ctx = {
+static ngx_http_module_t ngx_http_ojrouter_module_ctx = {
         NULL,
         NULL,
         NULL,
         NULL,
         NULL,
         NULL,
-        ngx_http_hello_world_create_loc_conf,
-        ngx_http_hello_world_merge_loc_conf
+        ngx_http_ojrouter_create_loc_conf,
+        ngx_http_ojrouter_merge_loc_conf
 };
 
 // Structure for the HelloWorld module, the most important thing
-ngx_module_t ngx_http_hello_world_module = {
+ngx_module_t ngx_http_ojrouter_module = {
         NGX_MODULE_V1,
-        &ngx_http_hello_world_module_ctx,
-        ngx_http_hello_world_commands,
+        &ngx_http_ojrouter_module_ctx,
+        ngx_http_ojrouter_commands,
         NGX_HTTP_MODULE,
         NULL,
         NULL,
@@ -60,13 +60,13 @@ ngx_module_t ngx_http_hello_world_module = {
         NGX_MODULE_V1_PADDING
 };
 
-static ngx_int_t ngx_http_hello_world_handler(ngx_http_request_t* r) {
+static ngx_int_t ngx_http_ojrouter_handler(ngx_http_request_t* r) {
     ngx_int_t rc;
     ngx_buf_t* b;
     ngx_chain_t out[2];
 
-    ngx_http_hello_world_loc_conf_t* hlcf;
-    hlcf = ngx_http_get_module_loc_conf(r, ngx_http_hello_world_module);
+    ngx_http_ojrouter_loc_conf_t* hlcf;
+    hlcf = ngx_http_get_module_loc_conf(r, ngx_http_ojrouter_module);
 
     r->headers_out.content_type.len = sizeof("text/plain") - 1;
     r->headers_out.content_type.data = (u_char*)"text/plain";
@@ -76,8 +76,8 @@ static ngx_int_t ngx_http_hello_world_handler(ngx_http_request_t* r) {
     out[0].buf = b;
     out[0].next = &out[1];
 
-    b->pos = (u_char*)"hello_world, ";
-    b->last = b->pos + sizeof("hello_world, ") - 1;
+    b->pos = (u_char*)"ojrouter, ";
+    b->last = b->pos + sizeof("ojrouter, ") - 1;
     b->memory = 1;
 
     b = ngx_pcalloc(r->pool, sizeof(ngx_buf_t));
@@ -91,7 +91,7 @@ static ngx_int_t ngx_http_hello_world_handler(ngx_http_request_t* r) {
     b->last_buf = 1;
 
     r->headers_out.status = NGX_HTTP_OK;
-    r->headers_out.content_length_n = hlcf->output_words.len + sizeof("hello_world, ") - 1;
+    r->headers_out.content_length_n = hlcf->output_words.len + sizeof("ojrouter, ") - 1;
     rc = ngx_http_send_header(r);
     if (rc == NGX_ERROR || rc > NGX_OK || r->header_only) {
         return rc;
@@ -100,10 +100,10 @@ static ngx_int_t ngx_http_hello_world_handler(ngx_http_request_t* r) {
     return ngx_http_output_filter(r, &out[0]);
 }
 
-static void* ngx_http_hello_world_create_loc_conf(ngx_conf_t* cf) {
-    ngx_http_hello_world_loc_conf_t* conf;
+static void* ngx_http_ojrouter_create_loc_conf(ngx_conf_t* cf) {
+    ngx_http_ojrouter_loc_conf_t* conf;
 
-    conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_hello_world_loc_conf_t));
+    conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_ojrouter_loc_conf_t));
     if (conf == NULL) {
         return NGX_CONF_ERROR;
     }
@@ -113,17 +113,17 @@ static void* ngx_http_hello_world_create_loc_conf(ngx_conf_t* cf) {
     return conf;
 }
 
-static char* ngx_http_hello_world_merge_loc_conf(ngx_conf_t* cf, void* parent, void* child) {
-    ngx_http_hello_world_loc_conf_t* prev = parent;
-    ngx_http_hello_world_loc_conf_t* conf = child;
+static char* ngx_http_ojrouter_merge_loc_conf(ngx_conf_t* cf, void* parent, void* child) {
+    ngx_http_ojrouter_loc_conf_t* prev = parent;
+    ngx_http_ojrouter_loc_conf_t* conf = child;
     ngx_conf_merge_str_value(conf->output_words, prev->output_words, "Nginx");
     return NGX_CONF_OK;
 }
 
-static char* ngx_http_hello_world(ngx_conf_t* cf, ngx_command_t* cmd, void* conf) {
+static char* ngx_http_ojrouter(ngx_conf_t* cf, ngx_command_t* cmd, void* conf) {
     ngx_http_core_loc_conf_t* clcf;
     clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
-    clcf->handler = ngx_http_hello_world_handler;
+    clcf->handler = ngx_http_ojrouter_handler;
     ngx_conf_set_str_slot(cf, cmd, conf);
     return NGX_CONF_OK;
 }
