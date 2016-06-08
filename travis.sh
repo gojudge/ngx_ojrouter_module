41dd7395d529e1799f2b9af962a5e92abea72ae9
Rex Lee(李俊) <duguying2008@gmail.com>
2016-06-08 17:01:42 +0800

    fixed script

--- travis.sh
+++ travis.sh
@@ -5,5 +5,5 @@ cd ~
 wget http://nginx.org/download/nginx-1.10.1.tar.gz
 tar zxf nginx-1.10.1.tar.gz
 cd nginx-1.10.1
-./configure --add-module=${#work_dir}
+./configure --add-module=${work_dir}
 make
